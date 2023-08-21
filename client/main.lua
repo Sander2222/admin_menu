ffa0d17c1134642e95a2a1f26be43a3d2042eba0
Sander <86840128+Sander2222@users.noreply.github.com>
2023-08-21 23:44:36 +0200

    renamed main file

---
+++ client/main.lua
@@ -0,0 +1,80 @@
+if Config.UseOpenKey then
+    RegisterKeyMapping('adminmenu', 'Open Adminmenu', 'keyboard', Config.DefaultOpenKey)
+end
+
+RegisterCommand('adminmenu', function(source, args)
+    ESX.TriggerServerCallback('admin_menu:callback:CheckGroup', function(CanOpen)
+        if CanOpen then
+            OpenMenu()
+        else
+            Config.ClientNotify('Server: Dieser Befehl ist nur für Admins verfügbar.')
+        end
+    end, args)
+end)
+
+function OpenMenu()
+    lib.registerContext({
+        id = 'some_menu',
+        title = 'Adminmenu',
+        options = {
+            {
+                title = 'Self',
+                description = 'Self Actions',
+                icon = 'image-user',
+                onSelect = function()
+                    OpenSelfMenu()
+                end,
+            },
+            {
+                title = 'Player',
+                description = 'Player Actions',
+                icon = 'circle',
+                onSelect = function()
+
+                end,
+            },
+            {
+                title = 'Vehicle',
+                description = 'Player Actions',
+                icon = 'circle',
+                onSelect = function()
+                    OpenVehMenu()
+                end,
+            },
+            {
+                title = 'Ban/Kick',
+                description = 'Player Actions',
+                icon = 'circle',
+                onSelect = function()
+                    print("Pressed the button!")
+                end,
+            },
+            {
+                title = 'Server',
+                description = 'Player Actions',
+                icon = 'circle',
+                onSelect = function()
+                    print("Pressed the button!")
+                end,
+            },
+            --   {
+            --     title = 'Menu button',
+            --     description = 'Takes you to another menu!',
+            --     menu = 'other_menu',
+            --     icon = 'bars'
+            --   },
+            --   {
+            --     title = 'Event button',
+            --     description = 'Open a menu from the event and send event data',
+            --     icon = 'check',
+            --     event = 'test_event',
+            --     arrow = true,
+            --     args = {
+            --       someValue = 500
+            --     }
+            --   }
+        }
+    })
+
+    lib.showContext('some_menu')
+end
