b46446a4275c4a9eec347f2fd7f3bcdcb48d5238
Sander <86840128+Sander2222@users.noreply.github.com>
2023-08-21 23:44:50 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,1 +1,25 @@
 Config = {}
+
+Config.UseOpenKey = true 
+Config.DefaultOpenKey = 'F5'
+Config.UseAceSystem = false
+Config.ESXGroups = {
+    'admin',
+    'mod',
+    'sup'
+}
+
+Config.ClientNotify = function(msg)
+    TriggerEvent('chat:addMessage', {
+        color = { 255, 0, 0 },
+        multiline = true,
+        args = { "Server", msg }
+    })
+end
+
+Config.WeaponList = {
+    {
+        name = 'WEAPON_PISTOL',
+        label = 'Pistol'
+    }
+}
