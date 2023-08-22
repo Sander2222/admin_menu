699ff1911a014c73ce718583245dbff06b10896d
Sander <86840128+Sander2222@users.noreply.github.com>
2023-08-22 21:32:00 +0200

    Create callback.lua

---
+++ server/callback.lua
@@ -0,0 +1,15 @@
+ESX.RegisterServerCallback('admin_menu:callback:CheckGroup', function(src, cb)
+
+    if CheckGroup(src, false) then
+        cb(true)
+    else 
+        cb(false)
+    end
+end)
+
+ESX.RegisterServerCallback('admin_menu:callback:GetAllitems', function(src, cb)
+
+    if CheckGroup(src, true) then
+        cb(ESX.Items)
+    end
+end)
