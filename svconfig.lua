b06f8b4778a4f32f5cd2369787d2d96118ddfa53
Sander <86840128+Sander2222@users.noreply.github.com>
2023-09-03 00:20:18 +0200

    add times to config

--- svconfig.lua
+++ svconfig.lua
@@ -1,3 +1,12 @@
 SVConfig = {}
 
 SVConfig.Webhooks = 'https://canary.discord.com/api/webhooks/1143301133452984350/WXMddiCR1qfQDLF5UfIEGItuMddEQT6PXnzPxrGNIdRsS0hTRFlJVGTCycYbe3LwIH0B'
+
+Config.Times = {
+    year = 31536000,
+    month = 2592000,
+    day = 86400,
+    hour = 3600,
+    minute = 60,
+    sec = 60
+}
