642dfd76ef9e0431caa7da0d22cc9ee7d6bbeea8
Sander <86840128+Sander2222@users.noreply.github.com>
2023-09-03 01:08:24 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -30,3 +30,12 @@ Config.PermaBanTime = {
 Config.BasicPlate = 'Test'
 Config.UnemployedJob = 'unemployed'
 Config.UnemployedJobGrade = 0
+
+Config.Times = {
+    year = 31536000,
+    month = 2592000,
+    day = 86400,
+    hour = 3600,
+    minute = 60,
+    sec = 60
+}
