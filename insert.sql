1b9e300836dac715e3988c6174eeefe7144b420f
Sander <86840128+Sander2222@users.noreply.github.com>
2023-09-04 01:06:41 +0200

    Create insert.sql

---
+++ insert.sql
@@ -0,0 +1,27 @@
+DELIMITER $$
+
+CREATE TRIGGER before_insert_users
+BEFORE INSERT ON users
+FOR EACH ROW
+BEGIN
+    DECLARE done INT DEFAULT 0;
+    DECLARE new_banid VARCHAR(10);
+
+    WHILE NOT done DO
+        SET new_banid = CONCAT(
+            LPAD(FLOOR(RAND() * 1000), 3, '0'), '-', 
+            SUBSTRING(MD5(RAND()), 1, 3)
+        );
+
+        IF NOT EXISTS (
+            SELECT 1 FROM users WHERE banid = new_banid
+        ) THEN
+            SET done = 1;
+        END IF;
+    END WHILE;
+
+    SET NEW.banid = new_banid;
+END;
+$$
+
+DELIMITER ;	
