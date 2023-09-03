672934024e298a9e0ded11716e8db7413ae0f3c1
Sander <86840128+Sander2222@users.noreply.github.com>
2023-09-03 17:43:52 +0200

    Create locals.lua

---
+++ locals.lua
@@ -0,0 +1,57 @@
+Locals = {
+    Self = {
+        SelfMenu = 'Self Menu',
+        ReviveMe = 'Revive me',
+        RevivedSelf = 'You have revived yourself',
+    
+        HealMe = 'self heal',
+        HealSelf = 'You have held yourself',
+    
+        ArmorMe = 'Give armor',
+        ArmorSelf = 'You have given yourself armor',
+    
+        KillMe = 'Kill self',
+        KillSelf = 'you killed yourself',
+
+        WeaponMe = 'Give weapon',
+        WeaponQuickSelection = 'Quick selection',
+        WeaponDsec = 'Quickly give weapon',
+        WeaponAdd = 'Add weapon',
+        WeaponName = 'Spawn name',
+        WeaponWhich = 'Which weapon',
+        WeaponAmmu = 'Ammunition',
+        WeaponAmmuDesc = 'How much ammunition',
+        WeaponEnterNumber = 'You must enter a valid number',
+        WeaponInfo = 'Weapon info',
+        WeaponWithESX = 'Not with ESX?',
+
+        ItemMe = 'Give item',
+        ItemQuickSelection = 'Quick selection',
+        ItemDesc = 'Quickly give item',
+        ItemName = 'Spawn name',
+        ItemWeight = ' Weight',
+        ItemCanRemove = 'Can remove',
+        ItemAdd = 'Add item',
+        ItemCount = 'Count',
+        ItemWhich = 'Which item',
+        ItemMuch = 'How much items',
+        ItemEnterNumber = 'You must enter a valid number',
+        ItemAddCount = 'Add count',
+
+        MoneyMe = 'Give money',
+        MoneyMeBank ='Give money bank',
+        MoneyAdd = 'Add money',
+        Money = 'Money',
+        MoneyMuch = 'how much money',
+        MoneyEnterNumber = 'You must enter a valid number',
+
+        NoclipMe = 'Noclip',
+
+        Webhooks = {
+            RevivedSelf = 'An admin has revived himself',
+            HealedSelf = 'An admin has healed himself',
+            ArmorSelf = 'An admin has given himself armor',
+            KilledSelf = 'An admin killed himself',
+        }
+    },
+}
