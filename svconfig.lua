9275139de354eb446e998931b92b7bcc3faf6b26
Sander <86840128+Sander2222@users.noreply.github.com>
2023-09-04 21:53:03 +0200

    add SVConfig.NotBannedRoles

--- svconfig.lua
+++ svconfig.lua
@@ -1,3 +1,9 @@
 SVConfig = {}
 
 SVConfig.Webhooks = 'https://canary.discord.com/api/webhooks/1143301133452984350/WXMddiCR1qfQDLF5UfIEGItuMddEQT6PXnzPxrGNIdRsS0hTRFlJVGTCycYbe3LwIH0B'
+
+SVConfig.NotBannedRoles = {
+    'admin',
+    'mod',
+    'sup'
+}
